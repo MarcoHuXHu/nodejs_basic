f377c920698f17ba619fb7d59fdcd7d418e1a23b
MarcoHuXHu <huxhu2333@gmail.com>
2017-02-17 15:37:52 +0800

    HelloWorld with nodejs

---
+++ HelloWorld/helloWorld_express.js
@@ -0,0 +1,15 @@
+// 
+var express = require('express');
+
+// 
+var app = express();
+
+// 
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
+
+app.listen(1111);
+
+// 
+console.log('Server running at 127.0.0.1:1111s');
